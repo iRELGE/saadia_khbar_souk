693e1afafd0b5b1ac73d8f5f103a33ae12dfd934
Rabia El Gouail <g.rabia@devaxy.com>
2024-03-24 16:31:39 +0000

    first commit

---
+++ saadia_khbar_souk_flutter/android/app/src/main/kotlin/com/example/saadia_khbar_souk_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.saadia_khbar_souk_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ saadia_khbar_souk_server/bin/main.dart
@@ -0,0 +1,7 @@
+import 'package:saadia_khbar_souk_server/server.dart';
+
+/// This is the starting point for your Serverpod server. Typically, there is
+/// no need to modify this file.
+void main(List<String> args) {
+  run(args);
+}
